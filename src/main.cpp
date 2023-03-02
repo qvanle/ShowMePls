8bd12eee3596953c36c7d5621dd1cdf4daa684e8
qvanle <vanle199tk@gmail.com>
2023-03-02 19:35:02 +0700

    test main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+
 int main()
 {
-    
-    std::cout << "hehe" << std::endl;
-
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized: " << SDL_GetError() << std::endl;
+    }else 
+    {
+        std::cout << "SDL video system is ready to go" << std::endl;
+    }
     return 0;
 }
