7b538c2c8fd90c3d26fe9561e926be4935ca5e2b
qvanle <vanle199tk@gmail.com>
2023-03-04 22:31:44 +0700

    create a window in SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,56 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <iostream>
 
-#include <SDL2/SDL.h>
-
-int main()
-{
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+bool init()
+{	
+    // returns true on success else return false
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
     {
-        std::cout << "SDL could not be initialized: " << SDL_GetError() << std::endl;
-    }else 
-    {
-        std::cout << "SDL video system is ready to go" << std::endl;
-    }
-    return 0;
+        std::cout << "Error when initialized SDL video" << std::endl;
+        return false;
+	}else std::cout << "Initializing SDL video success" << std::endl;
+    
+    return true;
 }
+
+int main(int argc, char *argv[])
+{
+    
+    if(!init()) 
+    {
+        std::cout << "Unable to initalize" << std::endl;
+        return 0;
+    }
+
+	SDL_Window* window = SDL_CreateWindow(
+                        "Show me please!",
+					    SDL_WINDOWPOS_CENTERED,
+					    SDL_WINDOWPOS_CENTERED,
+					    1000, 1000, 0
+                      );
+    
+    bool isOpen = true;
+
+    while(isOpen == true)
+    {
+        SDL_Event event;
+
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                isOpen = false;
+            }
+        }
+    }
+
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+	return 0;
+}
+
