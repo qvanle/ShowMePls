25ba4d4d1d40c78867784d39ffd7ddabc5360228
qvanle <vanle199tk@gmail.com>
2023-03-06 11:38:52 +0700

    initialization and add global variable

---
+++ include/Visualizer.hpp
@@ -0,0 +1,22 @@
+#ifndef VISUALIZER
+#define VISUALIZER
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
+#include <iostream>
+
+SDL_Window* window;
+
+bool Initialization()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Error when initalized SDL video" << std::endl; 
+        return false;
+    }else std::cout << "Initializing SDL video success" << std::endl;
+
+    return true;
+}
+
+#endif
