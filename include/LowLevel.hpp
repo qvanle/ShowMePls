f800c8e40003d19ea1de6dcefb2a76576dc31c29
qvanle <vanle199tk@gmail.com>
2023-03-06 19:35:43 +0700

    add assest diretory link

---
+++ include/LowLevel.hpp
@@ -0,0 +1,11 @@
+#ifndef LOWLEVEL
+#define LOWLEVEL
+
+#include <string>
+
+namespace GLOBAL
+{
+    extern const char* BGFolder; // background images folder
+    extern const char* ButFolder; // button images folder
+}
+#endif
---
+++ src/LowLevel.cpp
@@ -0,0 +1,4 @@
+#include <LowLevel.hpp>
+
+const char* GLOBAL::BGFolder = "asset/images/background/";
+const char* GLOBAL::ButFolder = "asset/images/buttons/";
