3a76d12640ff06a1c6aac3a7f4b68ed0a55ce6a4
qvanle <vanle199tk@gmail.com>
2023-03-06 14:24:13 +0700

    add openGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <glad/glad.h>
 #include <Visualizer.hpp>
 
 int main(int argc, char *argv[])
@@ -8,28 +6,15 @@ int main(int argc, char *argv[])
     {
         std::cout << "Unable to run!" << std::endl;
         return 0;
-    }else std::cout << "ShowMePls is running!" << std::endl;
-
-    SYS::window = SDL_CreateWindow(
-                        "Show me please!",
-					    SDL_WINDOWPOS_CENTERED,
-					    SDL_WINDOWPOS_CENTERED - 25,
-					    1280, 700, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL 
-                      );
+    }
     
-    bool isOpen = true;
+    SYS::Start();
 
-    while(isOpen == true)
+    while(SYS::isOpen)
     {
-        SDL_Event event;
-
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT)
-            {
-                isOpen = false;
-            }
-        }
+        SYS::Interacting();
+        glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     }
 
     SDL_DestroyWindow(SYS::window);
