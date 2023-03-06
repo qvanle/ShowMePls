7e30d351dfd379950075a3060f70e992ee39e6b0
qvanle <vanle199tk@gmail.com>
2023-03-06 14:22:55 +0700

    split into 2 file and setup openGL

--- include/Visualizer.hpp
+++ include/Visualizer.hpp
@@ -8,20 +8,11 @@
 
 namespace SYS
 {
-    SDL_Window* window;
-
-    bool Initialization()
-    {
-        if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        {
-            std::cout << "Error when initalized SDL video" << std::endl; 
-            return false;
-        }else std::cout << "Initializing SDL video success" << std::endl;
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-        return true;
-    }
+    extern SDL_Window* window;
+    extern SDL_GLContext context; 
+    extern bool isOpen;
+    bool Initialization();
+    void Start();
+    void Interacting();
 }
-
 #endif
---
+++ src/Visualizer.cpp
@@ -0,0 +1,56 @@
+#include <SDL2/SDL_video.h>
+#include <Visualizer.hpp>
+#include <glad/glad.h>
+
+bool SYS::isOpen = false;
+SDL_Window* SYS::window = nullptr;
+SDL_GLContext SYS::context;
+
+bool SYS::Initialization()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Error when initalized SDL video" << std::endl; 
+        return false;
+    }else std::cout << "Initializing SDL video success" << std::endl;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    return true;
+}
+
+void SYS::Start()
+{
+    std::cout << "ShowMePls is running" << std::endl;
+
+    SYS::isOpen = true;
+
+    SYS::window = SDL_CreateWindow(
+        "Show me please!", 
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED - 25,
+        1280, 700, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+    );
+    
+    SYS::context = SDL_GL_CreateContext(SYS::window);
+
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+}
+
+void SYS::Interacting()
+{
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            SYS::isOpen = false;
+        }
+    }
+}
+
