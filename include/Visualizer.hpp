1e0ab2956b1b74822041848e77d60c145ebd8288
qvanle <vanle199tk@gmail.com>
2023-03-06 12:55:28 +0700

    determine version of openGL(4.6)

--- include/Visualizer.hpp
+++ include/Visualizer.hpp
@@ -9,7 +9,7 @@
 namespace SYS
 {
     SDL_Window* window;
-    
+
     bool Initialization()
     {
         if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -17,7 +17,9 @@ namespace SYS
             std::cout << "Error when initalized SDL video" << std::endl; 
             return false;
         }else std::cout << "Initializing SDL video success" << std::endl;
-    
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         return true;
     }
 }
